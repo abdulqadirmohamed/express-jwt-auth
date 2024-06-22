3fa4ddafa2411c8c813de81452792a7c2765e5c2
abdulqadirmohamed <cajiibxaaji02@gmail.com>
2024-06-22 12:16:39 +0300

    Cookies Primer

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { mongoose } = require('mongoose');
 const path = require('path');
 const authRoute = require('./routes/authRoutes')
+const cookieParser = require('cookie-parser')
 
 const app = express();
 
@@ -20,12 +21,23 @@ mongoose.connect(dbUrl)
 // Middleware
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
     res.render('index', { title: 'Home' })
 })
 app.get('/smoothies', (req, res) => {
     res.render('smoothies', { title: 'Smoothies' })
+})
+
+// Cookies
+app.get('/set-cookies', (req, res)=>{
+    // res.setHeader('Set-Cookie', 'newUser=true');
+    res.cookie('newuser', false)
+    res.cookie('isEmployee', true, {maxAge: 1000 * 60 * 60 * 24, httpOnly: false})
+
+
+    res.send('you got a cookie')
 })
 
 app.use(authRoute)
