070d961d2f400214ee27c86ce6410d9da6ebc831
abdulqadirmohamed <cajiibxaaji02@gmail.com>
2024-06-26 22:34:10 +0300

    Mangoose validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,21 @@
 const User = require("../models/User")
 const jwt = require('jsonwebtoken')
 
+// Hadle errors
+
+const handleErrors = (err) => {
+    console.log(err.message, err.code)
+
+    // validate erros
+
+    if (err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({ properties }) => {
+           console.log(properties)
+        })
+    }
+
+}
+
 module.exports.signup_get = (req, res) => {
     res.render('signup')
 }
@@ -25,9 +40,9 @@ module.exports.signup_post = async (req, res) => {
     try {
         const user = await User.create({ email, password })
         res.status(201).json(user)
-    } catch (error) {
-        console.log(error);
-        res.status('400').send('error, user not created');
+    } catch (err) {
+        handleErrors(err)
+        res.status(400).send('error, user not created');
     }
 }
 
--- models/User.js
+++ models/User.js
@@ -1,23 +1,26 @@
 const mongoose = require('mongoose');
 const bcrypt = require("bcrypt")
+const {isEmail} = require('validator')
+
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please enter an email'],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email']
     },
     password: {
         type: String,
-        required: true,
-        minLength: 6
+        required: [true, 'Please enter a password'],
+        minLength: [6, 'Password must be at least 6 characters']
     }
 });
 
 
 // Fire function before doc save
-userSchema.pre('save', async function(next){
+userSchema.pre('save', async function (next) {
     const salt = await bcrypt.genSalt();
     this.password = await bcrypt.hash(this.password, salt)
     next()
